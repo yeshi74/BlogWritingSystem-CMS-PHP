fff21087abc1b85e46e52818560133b7e2551f11
yeshi74 <tentibet59@gmail.com>
2021-01-31 19:45:33 +0530

    superAdmin,userAdmin,website

---
+++ adminstats/connect.php
@@ -0,0 +1,5 @@
+<?php
+$dsn='mysql:host=localhost;dbname=blog_admin';//dbname and host
+$username='root';//username
+$password='';//password
+?>
---
+++ database/conn.php
@@ -0,0 +1,4 @@
+<?php
+//database connection
+($GLOBALS["___mysqli_ston"] = mysqli_connect("localhost","root","","blog_admin"));  //host,user,password,database
+?>
